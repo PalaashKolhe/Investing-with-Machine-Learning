bbde906f80219c72cd495a040cf03db10ead1a74
Palaash Kolhe <pkolhe@uwaterloo.ca>
2021-01-04 21:56:30 -0700

    exported data into csv file

--- testingDataSets.py
+++ testingDataSets.py
@@ -10,6 +10,7 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statsPath = path + '/_KeyStats'
     stockList = [x[0] for x in os.walk(statsPath)] # gather all stock names from folder
     #print(stockList)
+    df = pd.DataFrame(columns = ['Date', 'Unix', 'Ticker', 'DE Ratio'])
 
     for eachDir in stockList[1:]:
         eachFile = os.listdir(eachDir)
@@ -19,14 +20,15 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 dateStamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unixTime = time.mktime(dateStamp.timetuple())
                 fullPath = eachDir + '/' + file
-                #print(fullPath)
-
                 source = open(fullPath, 'r').read()
-                #print(source)
-                value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-                print(ticker+":", value)
-            time.sleep(15)
-
+                try:
+                    value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date':dateStamp, 'Unix':unixTime, 'Ticker':ticker, 'DE Ratio':value,}, ignore_index=True)
+                except Exception as e:
+                    pass
+    save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/','')+'.csv'
+    print(save)
+    df.to_csv(save)
 Key_Stats()
 
 
